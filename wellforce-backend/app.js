4be701ac0fea9d87a191934bcca42f1da2312f04
Abdirahman Ali <abdirahman.ali@abdirah-ltm4tx2.internal.salesforce.com>
2022-07-26 19:24:58 -0700

    Got the login and Register to work

---
+++ wellforce-backend/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const cors = require("cors");
+const security = require("./middleware/security");
+const authRouter = require("./routes/auth");
+const { PORT } = require("./config");
+
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+
+app.use(security.extractUserFromJwt);
+app.use("/auth", authRouter);
+
+// app.get("/", (req, res) => {
+//   res.status(200).send({ ping: "pong" });
+// });
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Good Post");
+});
+
+module.exports = app;
